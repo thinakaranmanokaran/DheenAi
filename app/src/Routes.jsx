db74889a785975e46f777bc5d94a6953002692d8
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2025-10-16 15:29:13 +0530

    Update Routes.jsx

--- app/src/Routes.jsx
+++ app/src/Routes.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 const Routes = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/DheenAi/">
             <PublicRoutes />
         </BrowserRouter>
     )
